c058f76f5992c49c20827ad8ee8717300ce48a09
Francisco Santos <francisco.santos@joby.aero>
2025-03-21 22:13:31 +0100

    add waiting time for shots

--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 import pygame
 from circleshape import CircleShape
 from shot import Shot
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -37,10 +38,13 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]: # move back
             self.move(-dt)
-        if keys[pygame.K_SPACE]: # shoot
+        if keys[pygame.K_SPACE] and self.timer <= 0: # shoot
             self.shoot()
+        if self.timer > 0:
+            self.timer -= dt
     
     def shoot(self):
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
 
